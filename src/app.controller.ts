58bbe476e3520f7a924b3922b346a1fe043cf3d5
Tran Nguyen Duy Khanh <tnduykhanhdng@gmail.com>
2024-02-19 14:53:48 +0700

    feat(auth): add JwtAuthGuard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,14 +8,16 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
-import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
   @Get()
@@ -26,6 +28,12 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async handleLogin(@Request() req: any) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: any) {
     return req.user;
   }
 }
